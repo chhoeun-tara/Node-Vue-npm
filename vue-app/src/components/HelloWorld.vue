a66f5f43ed9ff6cd3e0e390cbce3dccc123958c4
Tara <chhoeuntara2016@gmail.com>
2023-01-05 23:23:18 +0700

    add user

--- vue-app/src/components/HelloWorld.vue
+++ vue-app/src/components/HelloWorld.vue
@@ -1,7 +1,7 @@
 <template>
   <h1>User Info</h1>
   <ul>
-    <li>Username: {{user.username }}</li>
+    <li>Username: {{ user.username }}</li>
     <li>Password: ******</li>
     <li>Email: {{ user.email }}</li>
     <li>Phone: {{ user.phone }}</li>
@@ -9,12 +9,18 @@
 </template>
 
 <script lang="ts" setup>
-import User from 'tr-test-app';
+import UserModel from 'tr-test-app';
+import User from 'tr-test-app/user';
 
-const user: User = {
+const user: UserModel = {
   username: "admin",
   password: "@dm1n",
   email: "admin@email.com",
   phone: 10980801
 };
+
+const user2: User = {
+  name: 'Kok dara'
+}
+console.log(user2)
 </script>
